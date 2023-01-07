604977b13091d34111b8396237435fad583be31e
ZERO-94 <70008423+ZERO-94@users.noreply.github.com>
2023-01-07 23:34:00 +0700

    config push notif

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,33 @@ import {
 import "expo-dev-client";
 import { authentication } from "./services/firebase/firebase-config";
 import { onAuthStateChanged } from "firebase/auth";
-import * as FirebaseAuthTypes from "@firebase/auth-types";
 import BikerStack from "./routes/BikerStack";
 import UserStack from "./routes/UserStack";
 import {
   FSUser,
   getUser,
 } from "./services/firebase/firestore/userStore/userStore.operations";
+import messaging from "@react-native-firebase/messaging";
+import { Alert } from "react-native";
 
 export type Props = {};
 
-type User = FirebaseAuthTypes.User | null;
-
 export const UserContext = createContext<FSUser | null>(null);
+
+const requestUserPermission = async () => {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+  if (enabled) {
+    console.log("Authorization status:", authStatus);
+  }
+};
 
 const App: React.FC<Props> = () => {
   const [user, setUser] = useState<FSUser | null>(null);
+  const [notification, setNotification] = useState({});
 
   useEffect(() => {
     onAuthStateChanged(authentication, (user) => {
@@ -40,7 +51,21 @@ const App: React.FC<Props> = () => {
     });
   }, []);
 
-  console.log(user);
+  useEffect(() => {
+    requestUserPermission().then((res) => {
+      messaging()
+        .getToken()
+        .then((token) => {
+          console.log(token);
+        });
+    });
+
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <SafeAreaProvider initialMetrics={initialWindowMetrics}>
